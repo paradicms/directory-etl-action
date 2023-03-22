c1a699cb2194cae7d0b12e1a868d5129745d9194
Minor Gordon <minorg@server.fake>
2023-03-22 11:34:53 -0400

    update action.py and action.yml to fit with paradicms

--- action.py
+++ action.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-
+import dataclasses
 from dataclasses import dataclass
 from pathlib import Path
-from tempfile import TemporaryDirectory
 
 from paradicms_etl.extractors.markdown_directory_extractor import (
     MarkdownDirectoryExtractor,
@@ -12,41 +11,37 @@ from paradicms_etl.transformers.markdown_directory_transformer import (
     MarkdownDirectoryTransformer,
 )
 from paradicms_ssg.git_hub_action import GitHubAction
+from paradicms_ssg.github_actions_inputs import GitHubActionInputs
 from paradicms_ssg.models.root_model_classes_by_name import ROOT_MODEL_CLASSES_BY_NAME
 
 
-class Action(GitHubAction):
-    @dataclass(frozen=True)
-    class RequiredInputs(GitHubAction.RequiredInputs):
-        markdown_directory_path: str
+@dataclass(frozen=True)
+class _Inputs(GitHubActionInputs):
+    markdown_directory_path: str = dataclasses.field(
+        default=".",
+        metadata={"description": "Path to a directory of Markdown files"},
+    )
 
-    @dataclass(frozen=True)
-    class Inputs(GitHubAction.OptionalInputs, RequiredInputs):
-        pass
 
-    def __init__(self, *, inputs: Inputs, temp_dir_path: Path):
-        GitHubAction.__init__(
-            self,
-            optional_inputs=inputs,
-            required_inputs=inputs,
-            temp_dir_path=temp_dir_path,
-        )
-        self.__inputs = inputs
+class Action(GitHubAction[_Inputs]):
+    """
+    Generate a static site from a Paradicms Markdown directory.
+    """
 
     @classmethod
-    def main(cls):
-        with TemporaryDirectory() as temp_dir:
-            cls(inputs=cls.Inputs.from_args(), temp_dir_path=Path(temp_dir)).__main()
+    @property
+    def _inputs_class(cls):
+        return _Inputs
 
-    def __main(self):
+    def _run(self):
         Pipeline(
             extractor=MarkdownDirectoryExtractor(
-                markdown_directory_path=Path(self.__inputs.markdown_directory_path)
+                markdown_directory_path=Path(self._inputs.markdown_directory_path)
             ),
-            id=self.__inputs.pipeline_id,
+            id=self._inputs.pipeline_id,
             loader=self._create_loader(),
             transformer=MarkdownDirectoryTransformer(
-                pipeline_id=self.__inputs.pipeline_id,
+                pipeline_id=self._inputs.pipeline_id,
                 root_model_classes_by_name=ROOT_MODEL_CLASSES_BY_NAME,
             ),
         ).extract_transform_load()
