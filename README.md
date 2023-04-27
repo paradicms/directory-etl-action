ae52d190a5ac8c97c29528e0784a130ca3feda2e
Minor Gordon <minorg@server.fake>
2023-04-27 15:27:52 -0400

    update against Paradicms changes

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # `markdown-ssg-action`
 
-This GitHub Action extracts, transforms, and loads data from Paradicms-formatted Markdown files.
+This GitHub Action extracts, transforms, and loads data from files in a directory.
 
 ### Usage
 
-See [action.yml](action.yml) or the [user documentation](https://paradicms.org/docs/reference/github-actions#markdown-etl-action).
+See [action.yml](action.yml) or the [user documentation](https://paradicms.org/docs/reference/github-actions#directory-etl-action).
--- action.py
+++ action.py
@@ -4,40 +4,38 @@ from dataclasses import dataclass
 from pathlib import Path
 
 from paradicms_etl.etl_github_action import EtlGitHubAction
-from paradicms_etl.extractors.markdown_directory_extractor import (
-    MarkdownDirectoryExtractor,
+from paradicms_etl.extractors.directory_extractor import (
+    DirectoryExtractor,
 )
 from paradicms_etl.pipeline import Pipeline
-from paradicms_etl.transformers.markdown_directory_transformer import (
-    MarkdownDirectoryTransformer,
+from paradicms_etl.transformers.directory_transformer import (
+    DirectoryTransformer,
 )
 from paradicms_ssg.models.root_model_classes_by_name import ROOT_MODEL_CLASSES_BY_NAME
 
 
 class Action(EtlGitHubAction):
     """
-    Extract, transform, and load data from a Paradicms Markdown directory.
+    Extract, transform, and load data from files in a directory.
     """
 
     @dataclass(frozen=True)
     class Inputs(EtlGitHubAction.Inputs):
-        markdown_directory_path: str = dataclasses.field(
+        directory_path: str = dataclasses.field(
             default=".",
             metadata={"description": "Path to a directory of Markdown files"},
         )
 
-    def __init__(self, *, markdown_directory_path: str, **kwds):
+    def __init__(self, *, directory_path: str, **kwds):
         EtlGitHubAction.__init__(self, **kwds)
-        self.__markdown_directory_path = Path(markdown_directory_path)
+        self.__directory_path = Path(directory_path)
 
     def _run(self):
         Pipeline(
-            extractor=MarkdownDirectoryExtractor(
-                markdown_directory_path=self.__markdown_directory_path
-            ),
+            extractor=DirectoryExtractor(directory_path=self.__directory_path),
             id=self._pipeline_id,
             loader=self._loader,
-            transformer=MarkdownDirectoryTransformer(
+            transformer=DirectoryTransformer(
                 pipeline_id=self._pipeline_id,
                 root_model_classes_by_name=ROOT_MODEL_CLASSES_BY_NAME,
             ),
